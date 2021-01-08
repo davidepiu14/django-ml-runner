708460d54824acf3afa5ac0d9236f2e9446e561b
Davide Piu <61499360+davide-piu@users.noreply.github.com>
2021-01-08 14:52:39 +0100

    Update scraping_and_saving_tweets.py

--- miosito/sentiment/scraping_and_saving_tweets.py
+++ miosito/sentiment/scraping_and_saving_tweets.py
@@ -4,8 +4,7 @@ import tweepy
 from textblob import TextBlob
 from tweepy import OAuthHandler
 
-"""importo le credenziali dal file credentials.py e utilizzo tweepy per accedere all'API di twitter"""
-from credentials import TWITTER_ACCESS_TOKEN, \
+from credentials import TWITTER_ACCESS_TOKEN, \#importo le credenziali dal file credentials.py e utilizzo tweepy per accedere all'API di twitter
     TWITTER_CONSUMER_SECRET, \
     TWITTER_ACCESS_TOKEN_SECRET, \
     TWITTER_CONSUMER_KEY
@@ -40,17 +39,14 @@ def get_tweet_sentiment(tweet):
 
 
 def get_tweets_sentiment(query, count=10):
-    '''
-    Data una specifica query restituisce la sentiment del tweet
-    '''
+    """Data una specifica query restituisce la sentiment del tweet,richama API per ottenere i tweet, vengono solamente selezionati i tweet in lingua inglese, 
+        dato che count=10 vengono restituiti 10 tweet per pagina, non viene specificato se scaricare solo i più famosi 
+        o più i più recenti, di default viene preso un mix dei due"""
     tweets = []
     try:
-        """#si richama l'API per ottenere i tweet, vengono solamente selezionati i tweet in lingua inglese, 
-        dato che count=10 vengono restituiti 10 tweet per pagina, non viene specificato se scaricare solo i più famosi 
-        o più i più recenti, di default viene preso un mix dei due
-        """
-        fetched_tweets = tweepy.Cursor(twitterAPI.search, q=query,lang="en",tweet_mode='extended').items(count)
-        """parsing dei tweet, ogni tweet viene salvato i un dizionario che avrà le chiavi che vediamo sotto"""
+  
+        fetched_tweets = tweepy.Cursor(twitterAPI.search, q=query,lang="en",tweet_mode='extended').items(count)#parsing dei tweet, ogni tweet viene salvato i un dizionario che avrà le chiavi che vediamo sotto
+
         for tweet in fetched_tweets:
             parsed_tweet = {}
             parsed_tweet['text'] = tweet.full_text#testo dei tweet
@@ -73,14 +69,13 @@ def get_tweets_sentiment(query, count=10):
         print("Error : %s" % str(e))
 
 def dump_tweets(query, count):
-    """questa funzione richiama la funzione get_tweet_sentiment e salva i tweet all'interno di
+    """richiama get_tweet_sentiment e salva i tweet all'interno di
     un file csv"""
     tweets = get_tweets_sentiment(query=query,
                                   count=count)
     file_path = "tweets_%s.csv" % query.replace(' ', '_')#il nome del file comprende le parole oggetto della query
-    with open(file_path, 'a', encoding="utf8") as file:#'a' sta a significare che stiamo aggiungendo ad un file già presente e non stiamo sovrascrivendo
-        """DictWriter assegna ad ogni riga un dizionario, e all'interno di 'fieldname' ci sono le etichette """
-        csv_writer = csv.DictWriter(file,
+    with open(file_path, 'a', encoding="utf8") as file:#'a' sta a significare che stiamo aggiungendo e non sovrascrivendo
+        csv_writer = csv.DictWriter(file,#DictWriter assegna ad ogni riga un dizionario, e all'interno di 'fieldname' ci sono le etichette
                                     quotechar='"',
                                    fieldnames=['text', 'sentiment','name','location',
                                                'verified','retweet','date tweet','candidato'])
@@ -93,6 +88,6 @@ def dump_tweets(query, count):
 
 
 
-if __name__=="__main__":#con if __name__=="__main__": è possibile richiamare le funzioni anche in altri file
+if __name__=="__main__":# if __name__=="__main__": per richiamare le funzioni anche in altri file
     dump_tweets("donald trump",1000)#salvare i tweet di trump
     dump_tweets("joe biden",1000)#salvare i tweet di biden
