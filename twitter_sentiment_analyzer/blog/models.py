f9f3cdb78e2e64d07c4178545555f78c6d677baf
Davide Piu <davide.piu.94@gmail.com>
2022-09-13 00:06:48 +0200

    delete useless comments

--- twitter_sentiment_analyzer/blog/models.py
+++ twitter_sentiment_analyzer/blog/models.py
@@ -5,16 +5,13 @@ from django.urls import reverse
 
 
 class Post(models.Model):
-    """viene passato uno dei modelli standard di Django"""
-    title = models.CharField(max_length=100)#campo titolo di tipo char con max lunghezza 100
+    title = models.CharField(max_length=100)
     content = models.TextField()#contenuto del post
-    date_posted = models.DateTimeField(default=timezone.now)#data del post, automaticamente impostato alla data di creazione
-    author = models.ForeignKey(User, on_delete=models.CASCADE)#quando viene cancellato l'autore vengono cancellati anche tutti i post da lui scritti
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        """il metodo __str__ restituisce il titolo del post"""
         return self.title
 
     def get_absolute_url(self):
-        """django genera url"""
         return reverse('post-detail', kwargs={'pk': self.pk})
