bab7a770b444685508d93deb629044d2d56231d7
Davide Piu <davide.piu.94@gmail.com>
2022-10-18 22:24:12 +0200

    fix

--- django_project/django_sentiment/management/commands/save_polarity.py
+++ django_project/django_sentiment/management/commands/save_polarity.py
@@ -12,6 +12,7 @@ class Command(BaseCommand):
         try:
             tw = TwitterScraper()
             df_tweets_time_series = tw.compute_time_series_tweets()
+
             print(tw.db_save_tweet_polarity_point(df_tweets_time_series))
             self.stdout.write(self.style.SUCCESS('Successfully saved tweets polarity!'))
         
